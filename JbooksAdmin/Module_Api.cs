e920984e3c3f59f825ccaab77fac2b3b8bfcd11b
mami016 <mami016@naver.com>
2023-02-14 13:57:43 +0900

    [fix] aaa

--- JbooksAdmin/Module_Api.cs
+++ JbooksAdmin/Module_Api.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73f473da8e92547c6e571967c62519acb5c862d58bf1e47b33ce1cd08cfc2ad4
-size 128562
+oid sha256:4354a19bc33640b2d7655c73661cb2271e7d66323f06b9281f6f1c7e89c89b65
+size 129008
--- JbooksOpenApi/DbConn/BizNaver.cs
+++ JbooksOpenApi/DbConn/BizNaver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70fe240131cc2b3ebd9fa2ec4b7d191ad56c5d0006546ca0950f5f45e0c936ec
-size 118601
+oid sha256:53f44e35c147d28642dbdd2ae170a8e7afb50d4c19fcbab50046cd86caa1eaf4
+size 119503
