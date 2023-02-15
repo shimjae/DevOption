347763a39795e641e7cfe3c7133e3fc03f37a991
mami016 <mami016@naver.com>
2023-02-15 13:34:19 +0900

    [fix]aaa

--- JbooksAdmin/Module_PrdUpt.Designer.cs
+++ JbooksAdmin/Module_PrdUpt.Designer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba001ca54ea0dbcb252043b7c7953b9c86aebeea34ac777879b88956631d2cbe
-size 87272
+oid sha256:eb6943a13f50f74dc22feacb41221dc2d486d6919468b2d763ad773b2256d2b4
+size 87455
--- JbooksAdmin/Module_PrdUpt.cs
+++ JbooksAdmin/Module_PrdUpt.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01f01c2fccf8a96ef4aec8e001e2e0c7f9e8aa690ad187088e783f8b88a0f5e0
-size 108356
+oid sha256:96e1f44e39792e7983ae38e1156488e76f11735f9cbcb138fc4051406909dd16
+size 108305
