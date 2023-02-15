053f98da67ce6542d9b8f6a0ee107bd1b5242378
mami016 <mami016@naver.com>
2023-02-16 01:08:43 +0900

    [fix] aaa

---
+++ 11st/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51dfe93008af2193ecd287a8e3653367d0d1469bd4628e8c0dc99d7a5d91c808
+size 6196
---
+++ 11st/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc25a7841c112d07a91351de03a483e9a7267447fafd547e61e60c52d5cea5b5
+size 1507
